54da7322fc8920b7aa2d9b04186142fcc33a7774
MYSELF-SAYAN <116438215+MYSELF-SAYAN@users.noreply.github.com>
2025-07-20 20:38:27 +0530

    Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import {
 } from '@clerk/nextjs'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -36,6 +37,7 @@ export default function RootLayout({
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 
           {children}
+           <Analytics />
         </body>
       </html>
     </ClerkProvider>
